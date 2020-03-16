356e3e8b61350d46a1873fc235573cd78dc06472
Mark Tyneway <mark@purse.io>
2020-03-15 17:58:46 -0700

    app: add basic ui elements

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as tx from './tx';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField label="test" />
+        <TextField label="test" />
+        <TextField label="test" />
+        <TextField label="test" />
+        <TextField label="test" />
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
       </header>
     </div>
   );
