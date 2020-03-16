19742192c162a60d1efe87366d6b0ceb239a6147
Mark Tyneway <mark@purse.io>
2020-03-15 17:43:05 -0700

    fix build

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
           <code>src/App.js</code>
-          {' '}
           and save to reload.
         </p>
         <a
